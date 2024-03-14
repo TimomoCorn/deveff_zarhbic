3d95d9230a4b67a83580a13b9b2f0e8f33844ebe
TimomoCorn <119945189+TimomoCorn@users.noreply.github.com>
2024-03-14 14:40:26 +0100

    Add: Docstring en début de fichier

--- zarhbic.py
+++ zarhbic.py
@@ -1,3 +1,18 @@
+"""Module zarhbic.
+Un célèbre druide Breton du « Penn ar bed », nommé Zarhbic, vous remarquerez l'orthographe
+« rh » comme un enseignant d'info du 21ième siècle, a une drôle façon de faire les calculs
+arithmétiques même dans son état normal.
+
+Exemple:
+
+Calcul n°1 : 3 5 + donne 8
+Calcul n 2 : 4 7 + 3 * donne 33
+Calcul n 3 : 3 4 7 + * donne 33
+Calcul n 4 : 10 4 + 2 - donne 12
+Calcul n 5 : 2 10 4 + - donne -12
+"""
+
+
 def lecture_fichier(nom_fichier):
     """Lit un fichier et renvoie une liste de ses lignes."""
     with open(nom_fichier, 'r') as file:
